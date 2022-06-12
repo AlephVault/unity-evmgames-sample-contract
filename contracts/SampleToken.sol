bce768ad96514478812f7f79b2ccb5efa13f3fe1
Luis Masuelli <luis@datagran.io>
2022-06-11 21:13:38 -0500

    Adding a default minted amount on the sender address.

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SampleToken is ERC20 {
-  constructor() public ERC20("SMP", "Sample") {}
+  constructor() public ERC20("SMP", "Sample") {
+    _mint(msg.sender, 1000000000000000000);
+  }
 }
