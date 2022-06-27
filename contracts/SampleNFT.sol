eacac9601251d8af9d91a8bf9a2e1f422c892e14
Luis Masuelli <luis@datagran.io>
2022-06-26 21:03:30 -0500

    Adding sample contents to the NFT contract.

--- contracts/SampleNFT.sol
+++ contracts/SampleNFT.sol
@@ -5,5 +5,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SampleNFT is ERC721 {
   constructor() ERC721("Assets", "AST") {
+    this._safeMint(msg.sender, 0x111);
+    this._safeMint(msg.sender, 0x222);
+    this._safeMint(msg.sender, 0x333);
+    this._safeMint(msg.sender, 0x444);
+    this._safeMint(msg.sender, 0x555);
+    this._safeMint(msg.sender, 0x666);
+    this._safeMint(msg.sender, 0x777);
+    this._safeMint(msg.sender, 0x888);
+    this._safeMint(msg.sender, 0x999);
+    this._safeMint(msg.sender, 0xaaa);
   }
 }
